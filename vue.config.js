094f8a875837eb8a1f24d0752f72abc727b2df43
晓森 <1769811680@qq.com>
2021-05-13 15:29:10 +0800

    打包配置调整

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
   publicPath: './',//根路径
-  outputDir: 'dist',//构建输出目录
+  outputDir: 'sassExpert',//构建输出目录
   assetsDir:'assets',//静态资源目录(js,css,img,fonts)
   lintOnSave:false,//是否开启eslint保存检测，有效值：true||false||error
   runtimeCompiler: true,
